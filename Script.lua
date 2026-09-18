ecf6fa8f6fd1ca836160e8a14af994e19b088aa8
jesus3democla-ai <jesus3democla@gmail.com>
2026-09-18 15:31:30 -0500

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,7 @@
+echo "# Script-jesus" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/jesus3democla-ai/Script-jesus.git
+git push -u origin main
